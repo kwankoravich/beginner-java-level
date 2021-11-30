f790c78c4ab79416960ed70b11d2fb18232608c2
kwankoravich <41057176+kwankoravich@users.noreply.github.com>
2021-11-30 21:00:20 +0000

    Add files via upload

---
+++ Lab_01/callMethod.java
@@ -0,0 +1,13 @@
+package declareParameter;
+
+public class callMethod {
+	public static void main(String[] main) {
+		bark();
+	}
+	public static void bark(){
+		String dogName = "Bam";
+		System.out.println("The Dog name = " + dogName + "bark");
+	}
+	
+
+}
