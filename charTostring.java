56491b4f70a3ed68c40100125d9bb8ee03bd6f56
kwankoravich <41057176+kwankoravich@users.noreply.github.com>
2021-12-01 19:54:51 +0000

    Add files via upload

---
+++ charTostring.java
@@ -0,0 +1,10 @@
+package Lab_2;
+
+public class charTostring {
+	public static void main(String[] args) {
+		char[] myChar = {'K','W','A','N'};
+		String myWord = new String(myChar);
+		System.out.println(myWord);
+	}
+
+}
