be1b8632096fd3d9e0ad9cf797ca7783da3f4fae
kwankoravich <41057176+kwankoravich@users.noreply.github.com>
2021-12-03 19:54:19 +0000

    Add files via upload

--- Lab_04/Casting.java
+++ Lab_04/Casting.java
@@ -1,5 +1,12 @@
 package Lab_4;
 
 public class Casting {
+	public static void main(String[] args) {
+		Employee unk4 = new Employee("Anonymous","office","10000");
+		Employee ceo = new CEO("Anonymous","ceo",30000);
+		Employee prog = new Programmer("Anonymous","programmer",20000);
+		CEO ceo2 = (CEO) ceo; //cast 'ceo' variable into CEO class
+		Programmer prog2 = (Programmer) prog; // cast 'prog' variable into Programmer class
+	}
 
 }
